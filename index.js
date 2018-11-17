4e59df276b0a4ec7c0946da9636959cdde47a76b
goodjwon <jwon76@naver.com>
2018-11-17 10:24:00 +0900

    nomad coders vanilla-script init

---
+++ index.js
@@ -0,0 +1,16 @@
+//alert('Im working');
+console.log('Im working')
+
+
+
+let a = 100;
+console.log('let a is 100'+a);
+
+a= 10;
+console.log('change a is 10'+a);
+
+const b = 100;
+console.log(b);
+
+b = 200
+console.log(b);
