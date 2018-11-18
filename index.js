914fe945349a37f09a5627ee3af806c57954aa0d
goodjwon <jwon76@naver.com>
2018-11-18 11:58:26 +0900

    요구사항 처리 red => click => yellow

--- index.js
+++ index.js
@@ -1,39 +1,29 @@
-
+/*
+    1. title 처음에는 빨강 색인다.
+    2. title 에 클릭하면 색을 파랑색으로 바꾼다.
+    3. 다시 클릭하면 원래 색으로 바꾼다.
+*/
 
 const title = document.getElementById('title'); //get DOM getElementById
 
+const BASE_COLOR = 'red';
+const OTHER_COLOR = 'yellow';
 
-
-console.log(title);
-console.dir(title);
-
-
-title.innerHTML = "Hi from JS";
-title.style.color = "red";
-
-console.dir(title);
-
-document.title = "I love u";
-
-
-const title2 = document.querySelector('#title');    //get DOM get querySelector
-title2.innerHTML = "Hi from JS selector";
-title.style.color = "white";
-
-
-
-function handleResize(event){
-    console.log(event)
+function init(){
+    title.style.color = BASE_COLOR;
+    title.addEventListener("click", changeColor);
 }
 
-window.addEventListener("resize", handleResize);
+init();
 
-
-function handleClick(){
-    title.style.color ="green";
+function changeColor(){
+    if(title.style.color === BASE_COLOR){
+        title.style.color = OTHER_COLOR;
+    } else {
+        title.style.color = BASE_COLOR;
+    }
 }
 
-title.addEventListener("click", handleClick);
 
 /**
  Event List
@@ -54,5 +44,8 @@ title.addEventListener("click", handleClick);
      unload – 링크를 클릭해서 다른 페이지로 이동하거나 브라우저 탭을 닫을 때 혹은 브라우저 창을 닫을 때 이벤트가 발생한다.
      change – 폼 필드의 상태가 변경되었을 때 발생한다. 라디오 버튼을 클릭하거나 셀렉트 박스에서 값을 선택하는 경우를 예로 들수 있다.
  */
+
+
+
 
 
