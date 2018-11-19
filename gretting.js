53b5426d455faa65d1d12e71868c110dee36775a
goodjwon <jwon76@naver.com>
2018-11-19 15:22:55 +0900

    say hello {username}

---
+++ gretting.js
@@ -0,0 +1,30 @@
+
+const form = document.querySelector('.js-form');
+const input = form.querySelector('input');
+const greeting = document.querySelector(".js-greetings")
+
+const USER_LS = "currentUser";
+const SHOWING_CN = "showing";
+
+function printGreetingName(text) {
+    form.classList.remove(SHOWING_CN);
+    greeting.classList.add(SHOWING_CN);
+    greeting.innerText = `Hello ${text}`;
+}
+
+function loadName() {
+    const currentUser = localStorage.getItem(USER_LS);
+
+    if(currentUser===null){
+
+    }else{
+        printGreetingName(currentUser);
+    }
+
+}
+
+function init() {
+    loadName();
+}
+
+init();
